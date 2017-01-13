6f9f175a569e0c895fce437050e8989fef95a01a
Andy Prier <andyprier@mindsetconsulting.com>
2017-01-13 05:54:57 -0700

    Path fixing

--- webapp/test/phantomjs-test.js
+++ webapp/test/phantomjs-test.js
@@ -1,7 +1,7 @@
 var fs = require('fs'),
 	system = require('system'),
 	page = require('webpage').create(),
-	file = fs.absolute((system.args.length > 1 && system.args[1]) || '/webapp/test/unit/unitTests.qunit.html');
+	file = fs.absolute((system.args.length > 1 && system.args[1]) || 'webapp/test/unit/unitTests.qunit.html');
 
 
 page.onConsoleMessage = function (msg) {
